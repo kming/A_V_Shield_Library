e90295ec94403459e1ac100d07feada3256e10e5
Kming <Keiming.kwong@gmail.com>
2014-01-09 22:21:46 -0500

    Adding unit_test_main for testing infrastructure

---
+++ AV_Shield/unit_test/unit_test_main.py
@@ -0,0 +1,30 @@
+import sys 
+import os
+import glob
+import subprocess
+
+list_of_tests = ["audio_wav_object", "generic", "setup"]
+
+SUCCESSFUL = 0
+FAIL = 1
+
+def run_all_test():
+	verbose = 1 # use a hardcode for now, implement getopt later
+	status = SUCCESSFUL
+
+	# Go through all unit test and call the unit test file
+	for module in list_of_tests:
+		print "Running %s" % module 
+		# Call the module's main test 
+		unit_test_str = "AV_Shield.unit_test.unit_test_" + module
+		m_obj = __import__ (unit_test_str)
+		print dir(m_obj)
+		status = getattr(m_obj, 'main')()
+		
+	
+		# Check testing results
+		if status == SUCCESSFUL:
+			print "  - Passed" 
+		else:
+			print "  - Failed" 
+	return status
